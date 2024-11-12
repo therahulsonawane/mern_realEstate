5c4e900c56168f15485dce8352a9c4cfc03f450e
therahulsonawane <rahulsonwane7066@gmail.com>
2024-11-12 20:36:23 +0530

    signup adn signin functionality added

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/profile";
+import Header from "./component/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// mongodb connection
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
+
+app.use('/server/user',userRouter)
+app.use('/server/auth',authRouter)
+
+// This is middleware that will be used to 
+app.use((err, req,res,next)=>{
+const statusCode = err.statusCode || 500;
+const message = err.message || "Internal Server Error"
+return res.status(statusCode).json({
+  success :false,
+  statusCode,
+  message
+})})
