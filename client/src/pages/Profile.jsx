d9e2384141546a75e337804c38499d6ee571a3d8
therahulsonawane <rahulsonwane7066@gmail.com>
2024-11-22 17:44:10 +0530

    i have eidted the placeholder

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 export default function profile() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>profile</div>
-  )
+    <div>
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="
+        rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input type="text" placeholder="text" />
+      </form>
+    </div>
+  );
 }
